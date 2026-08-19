c597b7f12aa060e57618adbb55d1df8636dc26af
muhammadali <muhammadali@gmail.com>
2026-08-19 13:10:30 -0400

    Week 6: Terraform infra + Ansible config management for Hands-on Activity

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -16,9 +16,9 @@ resource "aws_vpc" "main" {
 # -------------------------------
 resource "aws_subnet" "main" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block               = var.subnet_cidr
-  availability_zone        = var.availability_zone
-  map_public_ip_on_launch  = true
+  cidr_block              = var.subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "week6-subnet"
